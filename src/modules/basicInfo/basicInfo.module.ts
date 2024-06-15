827182c72f5c0a6293fc9c187c285481ffbc09f4
Nilesh Mistry <nileshmistry@192.168.1.4>
2024-06-16 03:30:08 +0800

    Fixed providers issues

--- src/modules/basicInfo/basicInfo.module.ts
+++ src/modules/basicInfo/basicInfo.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BasicInfoService } from './basicInfo.service';
+import { ConfigService } from '@nestjs/config';
+import { HttpClient } from '@infra/http/http.service';
 
 @Module({
-  providers: [BasicInfoService],
+  providers: [
+    ConfigService,
+    { provide: 'BasicInfoService', useClass: BasicInfoService },
+    { provide: 'HttpClient', useClass: HttpClient },
+  ],
 })
 export class BasicInfoModule {}
--- src/modules/fund/fund.module.ts
+++ src/modules/fund/fund.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FundService } from './fund.service';
+import { ConfigService } from '@nestjs/config';
+import { HttpClient } from '@infra/http/http.service';
 
 @Module({
-  providers: [FundService],
+  providers: [
+    ConfigService,
+    { provide: 'FundService', useClass: FundService },
+    { provide: 'HttpClient', useClass: HttpClient },
+  ],
 })
 export class FundModule {}
